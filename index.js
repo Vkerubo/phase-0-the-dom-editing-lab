0daf07a1bed9362d28b90f14ac8641457e1ea833
Valentine <kerubovalentine1@gmail.com>
2023-03-21 20:34:16 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+// From the Three Dog Night song: "Joy to the World (Jeremiah was a Bullfrog)"
+const lineOne = "Joy to the world";
+const lineTwo = "All the boys and girls";
+const lineThree = "Joy to the fishes in the deep blue sea";
+const lineFour = "Joy to you and me";
+
+// The '\n' inserts a new line into the string
+const chorus = `${lineOne}\n${lineTwo}\n${lineThree}\n${lineFour}`;
+
+chorus;
